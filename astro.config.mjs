2a9c0bacf2f0290c6e72652c6703a8935d3b055f
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2024-01-07 18:24:00 +0100

    Add base path for Open IPC Docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ihsenbouallegue.github.io",
+  base: "/openipc-docs/",
   integrations: [
     starlight({
       title: "Open IPC Docs",
